fd157bde53f1defddf37ad2bf42e9accb3aca6a5
hamed <hamed.ma7@gmail.com>
2018-06-02 16:46:13 +0430

    added video and interstitial

--- app/src/main/java/com/hmomeni/deematest/MainActivity.kt
+++ app/src/main/java/com/hmomeni/deematest/MainActivity.kt
@@ -1,14 +1,56 @@
 package com.hmomeni.deematest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.deema.adcontainer.InterstitialAdContainer
+import com.deema.adcontainer.VideoContainer
+import com.deema.adcontainer.listener.InterstitialListener
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-		banner.loadAd()
-	}
+    lateinit var interstitialAdContainer: InterstitialAdContainer
+    lateinit var videoContainer: VideoContainer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        videoContainer = VideoContainer(this, "40")
+        interstitialAdContainer = InterstitialAdContainer(this, "39")
+
+        loadInter.setOnClickListener {
+            interstitialAdContainer.loadAd(object : InterstitialListener {
+                override fun onSuccess() {
+                    interstitialAdContainer.showAd()
+                }
+
+                override fun onFailure(p0: Exception?) {
+                    p0?.printStackTrace()
+                }
+
+                override fun closed() {
+                }
+            })
+        }
+
+
+        videoBtn.setOnClickListener {
+            videoContainer.loadAd(object : InterstitialListener {
+                override fun onSuccess() {
+                    videoContainer.showAd()
+                }
+
+                override fun onFailure(p0: Exception?) {
+                    p0?.printStackTrace()
+                }
+
+                override fun closed() {
+                }
+            })
+        }
+
+        banner.loadAd()
+    }
 }
